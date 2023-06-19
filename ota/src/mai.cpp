ae521759800f40d38f01c89597cc2f3531b6673a
KESANA RAJA NARENDRA MANIKANTA <109544031+arduinojinarendra@users.noreply.github.com>
2023-06-19 11:26:30 +0530

    Add files via upload

---
+++ ota/src/mai.cpp
@@ -0,0 +1,77 @@
+
+//Code by GVV Sharma
+//April 29, 2022
+//https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+//Seven segment diplay OTA
+//
+//----------------------Skeleton Code--------------------//
+
+//           LCD interfacing With ESP32     //
+
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+int a=0,b=0,c=0,d=0;
+int A,B,C,D;
+
+// set the LCD number of columns and rows
+ 
+//    Can be client or even host   //
+#ifndef STASSID
+#define STASSID "raja"  // Replace with your network credentials
+#define STAPSK  "123456789"  
+#endif
+
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+
+
+
+void OTAsetup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(5000);
+    ESP.restart();
+  }
+  ArduinoOTA.begin();
+}
+
+void OTAloop() {
+  ArduinoOTA.handle();
+}
+
+//-------------------------------------------------------//
+int a,b,f;
+
+
+void setup(){
+  OTAsetup();
+
+  
+    pinMode(15,INPUT);
+    pinMode(17,INPUT);
+    pinMode(19,OUTPUT);
+    
+}
+
+void loop() {
+  OTAloop();
+  delay(10);  // If no custom loop code ensure to have a delay in loop
+  //-------------------//
+  // Custom loop code  //
+  //-------------------//
+  a=digitalRead(15);
+  b=digtalRead(17);
+  y=!a|!b;
+  digitalWrite(19,y);
+  
+  
+}
+
+
